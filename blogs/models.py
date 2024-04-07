d73ea6d0c2e91c3d9c44b5c4b141bc67304deda3
GideonMissoy <giddykips2030@gmail.com>
2024-04-07 10:54:10 +0300

    Adding models and sample templates.

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.CharField(max_length=50)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Category, Post, Comment
 
 # Create your views here.
+def indexView(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {"posts": posts}
+    return render(request, 'blogs/index.html', context)
