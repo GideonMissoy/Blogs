85968fa869de2d57acf7b6046227a2fddeb2ec4c
GideonMissoy <giddykips2030@gmail.com>
2024-04-10 21:57:19 +0300

    Updates.

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', indexView, name='index'),
+    path('detail/', blogDetail, name='detail'),
+    path('category/', blogCategory, name='category'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -6,3 +6,22 @@ def indexView(request):
     posts = Post.objects.all().order_by("-created_on")
     context = {"posts": posts}
     return render(request, 'blogs/index.html', context)
+
+def blogDetail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        }
+    return render(request, 'blogs/detail.html', context)
+
+def blogCategory(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, 'blog/category.html', context)
